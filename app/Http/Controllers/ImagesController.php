2b73950dba016e4e839b6784a5bdd8b7d4378734
dionation <dionation@gmail.com>
2018-10-09 09:34:26 +0200

    version normal

---
+++ app/Http/Controllers/ImagesController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+// use Intervention\Image\ImageManager;
+use ImageIntervention;
+use App\Image;
+use Illuminate\Http\Request;
+
+class ImagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $image = $request->file('image');
+        $filename = time().$image->hashName();
+        $resize = ImageIntervention::make($image)->resize(100, 100);
+        $resize->save('images/'.$filename);
+
+        $table = new Image;
+        $table->name = $filename;
+        $table->save();
+        
+        return redirect()->back();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Image  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Image $image)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Image  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Image $image)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Image  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Image $image)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Image  $image
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Image $image)
+    {
+        //
+    }
+}
